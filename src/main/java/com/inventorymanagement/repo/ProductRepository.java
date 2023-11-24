0ca2b7ce8401a952e280a8085ab6d9e4e611a62d
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-24 19:17:49 +0530

    Delete src directory

--- src/main/java/com/inventorymanagement/repo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inventorymanagement.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.inventorymanagement.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
