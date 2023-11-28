f7bbc5d25acd930b264029034b98c0d6eab96f50
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-28 11:01:41 +0530

    Update ProductController.java

--- src/main/java/com/inventorymanagement/controller/ProductController.java
+++ src/main/java/com/inventorymanagement/controller/ProductController.java
@@ -26,33 +26,4 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
-	@GetMapping
-	public ResponseEntity<List<ProductDTO>> getAllProducts() {
-		List<ProductDTO> products = productService.getAllProducts();
-		return new ResponseEntity<>(products, HttpStatus.OK);
-	}
-
-	@GetMapping("/{id}")
-	public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
-		ProductDTO product = productService.getProductById(id);
-		return new ResponseEntity<>(product, HttpStatus.OK);
-	}
-
-	@PostMapping
-	public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO productDTO) {
-		ProductDTO createdProduct = productService.createProduct(productDTO);
-		return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
-	}
-
-	@PutMapping("/{id}")
-	public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductDTO productDTO) {
-		ProductDTO updatedProduct = productService.updateProduct(id, productDTO);
-		return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
-	}
-
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-		productService.deleteProduct(id);
-		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-	}
 }
