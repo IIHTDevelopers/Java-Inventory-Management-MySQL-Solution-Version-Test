c07f15271df2f4ff00cbe03aa7055c1872015bb2
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-28 11:00:57 +0530

    Update SellController.java

--- src/main/java/com/inventorymanagement/controller/SellController.java
+++ src/main/java/com/inventorymanagement/controller/SellController.java
@@ -25,28 +25,4 @@ public class SellController {
 	@Autowired
 	private SellService sellService;
 
-	@GetMapping
-	public ResponseEntity<List<SellDTO>> getAllSells() {
-		List<SellDTO> sells = sellService.getAllSells();
-		return new ResponseEntity<>(sells, HttpStatus.OK);
-	}
-
-	@GetMapping("/{id}")
-	public ResponseEntity<SellDTO> getSellById(@PathVariable Long id) {
-		SellDTO sell = sellService.getSellById(id);
-		return new ResponseEntity<>(sell, HttpStatus.OK);
-	}
-
-	@PostMapping
-	public ResponseEntity<SellDTO> createSell(@Valid @RequestBody SellDTO sellDTO) {
-		SellDTO createdSell = sellService.createSell(sellDTO);
-		return new ResponseEntity<>(createdSell, HttpStatus.CREATED);
-	}
-
-	@GetMapping("/products-sold-last-month")
-	public ResponseEntity<Map<String, Integer>> getProductsSoldLastMonth() {
-		Map<String, Integer> productSalesMap = sellService.getProductsSoldInLastMonth();
-		return new ResponseEntity<>(productSalesMap, HttpStatus.OK);
-	}
-
 }
