8beb8d323587701dd61788617201da068b8781ff
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-28 10:57:22 +0530

    Update SellRepository.java

--- src/main/java/com/inventorymanagement/repo/SellRepository.java
+++ src/main/java/com/inventorymanagement/repo/SellRepository.java
@@ -9,6 +9,4 @@ import org.springframework.data.jpa.repository.Query;
 import com.inventorymanagement.entity.Sell;
 
 public interface SellRepository extends JpaRepository<Sell, Long> {
-	@Query("SELECT s " + "FROM Sell s " + "WHERE s.orderDate BETWEEN :startDate AND :endDate")
-	List<Sell> findSellsInLastMonth(Date startDate, Date endDate);
 }
